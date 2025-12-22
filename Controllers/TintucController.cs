abdeeead9e637bfe925e5c21a2f7af618b20f604
nowhere1998 <dinhthien6270790@gmail.com>
2025-12-22 09:13:04 +0700

    update 22/12

--- Controllers/TintucController.cs
+++ Controllers/TintucController.cs
@@ -1,19 +1,49 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyShop.Models;
 
 namespace MyShop.Controllers
 {
 	public class TintucController : Controller
 	{
-		[Route("tin-tuc")]
-		public IActionResult Index()
+		private readonly DbMyShopContext _context;
+
+        public TintucController(DbMyShopContext context)
+        {
+            _context = context;
+        }
+        [Route("tin-tuc")]
+		[Route("tin-tuc/page/{page:int}")]
+		public IActionResult Index(int page = 1)
 		{
-			return View();
+			int pageSize = 6;
+
+			var query = _context.News
+				.Where(x => x.Status == 1)
+				.OrderByDescending(x => x.Id);
+
+			int totalItems = query.Count();
+			int totalPages = (int)Math.Ceiling((double)totalItems / pageSize);
+
+			var news = query
+				.Skip((page - 1) * pageSize)
+				.Take(pageSize)
+				.ToList();
+
+			ViewBag.CurrentPage = page;
+			ViewBag.TotalPages = totalPages;
+
+			return View(news);
 		}
 
-        [Route("chi-tiet-tin-tuc")]
-        public IActionResult Chitiet()
+		[Route("chi-tiet-tin-tuc/{slug}")]
+        public IActionResult Chitiet(string slug = "")
         {
-            return View("chi-tiet-tin-tuc");
+			var post = new News();
+			if(string.IsNullOrEmpty(slug))
+			{
+				post = _context.News.Where(x => x.Status == 1).FirstOrDefault(post => post.Slug == slug);
+			}
+            return View("chi-tiet-tin-tuc", post);
         }
     }
 }
