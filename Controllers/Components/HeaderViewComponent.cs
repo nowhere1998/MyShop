4ab3ba8bd91f25f97df302d22d1bb442023fc007
nowhere1998 <dinhthien6270790@gmail.com>
2025-12-24 11:10:52 +0700

    update 24/12

---
+++ Controllers/Components/HeaderViewComponent.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyShop.Models;
+
+namespace MyShop.Controllers.Components
+{
+    public class HeaderViewComponent:ViewComponent
+    {
+        private readonly DbMyShopContext _context;
+
+        public HeaderViewComponent(DbMyShopContext context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var config = _context.Configs.FirstOrDefault();
+
+            if (config == null)
+            {
+                config = new Config
+                {
+                    MailSmtp = string.Empty,
+                    MailInfo = string.Empty,
+                    MailNoreply = string.Empty,
+                    MailPassword = string.Empty,
+                    PlaceHead = string.Empty,
+                    PlaceBody = string.Empty,
+                    GoogleId = string.Empty,
+                    Contact = string.Empty,
+                    Copyright = string.Empty,
+                    Title = string.Empty,
+                    Description = string.Empty,
+                    Keyword = string.Empty,
+                    Lang = string.Empty,
+                    HotLine = string.Empty,
+                    YoutubeLink = string.Empty,
+                    PicasaLink = string.Empty,
+                    FlickrLink = string.Empty,
+                    SocialLink1 = string.Empty,
+                    SocialLink2 = string.Empty,
+                    SocialLink3 = string.Empty,
+                    SocialLink4 = string.Empty,
+                    SocialLink5 = string.Empty,
+                    SocialLink6 = string.Empty,
+                    SocialLink7 = string.Empty,
+                    SocialLink8 = string.Empty,
+                    SocialLink9 = string.Empty
+                };
+            }
+
+            return View("Default", config);
+        }
+    }
+}
