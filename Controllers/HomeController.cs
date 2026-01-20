e07e0c6e2cffd3c7591d5427c3860fd9b03fe627
FIRST_NAME LAST_NAME <lucvanthai16@gmail.com>
2026-01-20 09:39:39 +0700

    update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -38,7 +38,40 @@ namespace MyShop.Controllers
                 .OrderByDescending (x => x.Id)
                 .Where(x => x.Status == 1)
                 .ToList();
-            
+
+            var pagesL1 = _context.Pages
+                .Where(l1 =>
+                    l1.Level != null &&
+                    l1.Level.Length == 5 &&
+                    l1.Active == 1 &&
+                    (l1.Position == 2 || l1.Position == 4))
+                .OrderBy(l1 => l1.Ord)
+                .Select(l1 => new PageL1
+                {
+                    Page = l1,
+
+                    // Có c?p 2
+                    HasChild = _context.Pages.Any(l2 =>
+                        l2.Level != null &&
+                        l2.Level.Length == 10 &&
+                        l2.Level.StartsWith(l1.Level) &&
+                        l2.Active == 1 &&
+                        (l2.Position == 2 || l2.Position == 4)
+                    ),
+
+                })
+                .ToList();
+
+            var pagesL2 = _context.Pages
+                .OrderBy(x => x.Ord)
+                .Where(x => x.Level != null
+                    && x.Level.Length == 10
+                    && x.Active == 1
+                    && (x.Position == 2 || x.Position == 4))
+                .ToList();
+
+            ViewBag.PagesL1 = pagesL1;
+            ViewBag.PagesL2 = pagesL2;
             ViewBag.News = news;
 			ViewBag.Categories = categories;
             ViewBag.Products = products;
